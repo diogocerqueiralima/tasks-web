b35861017f632c310f057a0cfb1851e9aad2008c
diogodelima <diogocerqueiradeelima@gmail.com>
2025-01-16 11:42:48 +0000

    feat: add main page and toast into header when try access task whithout login

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { AuthenticationContext } from "@/app/context/AuthenticationContext"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import styles from "./page.module.css";
 import 'boxicons/css/boxicons.min.css';
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Toast from "../toast/Toast";
 
 export default function Header() {
 
@@ -13,20 +15,31 @@ export default function Header() {
     if (!authContext)
         return
 
-    const headers = [
-        { id: "1", name: "Home", icon: "bx bx-home-alt-2", path: "/" },
-        { id: "2", name: "Tasks", icon: "bx bx-notepad", path: "/tasks" }
-    ]
+    const [showToast, setShowToast] = useState(false)
+    const router = useRouter()
+    const navigateToTasks = () => {
+
+        if (!authContext.isAuthenticated()) {
+            setShowToast(true)
+            return
+        }
+
+        router.push("/tasks")
+    }
 
     return (
 
         <header className={styles.header}>
 
+            { showToast && <Toast message="You need to log in to an account" icon="bx bx-info-circle" time={5000} removeToast={() => { setShowToast(false) }} /> }
+
             <img src="logo.webp"></img>
             
             <ul>
 
-                { headers.map(h => <li key={h.id}> <Link href={h.path}><i className={h.icon}></i> <span>{h.name}</span></Link> </li>) }
+                <li> <Link href="/"> <i className="bx bx-home-alt-2" /> <span>Home</span> </Link> </li>
+
+                <li onClick={navigateToTasks}> <i className="bx bx-notepad" /> <span>Tasks</span> </li>
 
                 { (authContext.isAuthenticated() ? <li onClick={authContext.logout}> <i className="bx bx-user"></i> <span>Logout</span> </li> : <li onClick={authContext.login}> <i className="bx bx-user"></i> <span>Login</span> </li>) }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,24 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-        <Header />
+
+      <Header />
+    
+      <main>
+
+        <div className={styles.text}>
+
+          <h1>Achieve More, Stress Less: Your Personal Task Manager Awaits</h1>
+          <p>
+            Stay organized and take control of your day with a simple, 
+            intuitive task manager designed to help you focus on what truly matters. 
+            Plan, prioritize, and accomplish your goals effortlessly!
+          </p>
+
+        </div>
+
+      </main>
+    
     </div>
   );
 }
